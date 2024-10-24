5ca24e2c89e7517a7918bdf53ec68ec9ed5a4b64
Haroun EL ALAMI <haroun.dev@gmail.com>
2024-10-24 20:20:52 +0200

    feat(os): add sdcard option

--- start-emulator.sh
+++ start-emulator.sh
@@ -2,8 +2,20 @@
 
 # Check if the .first-boot-done file exists
 if [ -f /data/.first-boot-done ]; then
-  RAMDISK_PATH="-ramdisk /data/android.avd/ramdisk.img"
+  RAMDISK="-ramdisk /data/android.avd/ramdisk.img"
 fi
 
+# Define the SD card path
+SDCARD_PATH="/data/android.avd/sdcard.img"
+SDCARD="-sdcard $SDCARD_PATH"
+# Initialize the SDCARD_PATH only if CREATE_SDCARD is true and the sdcard doesn't exist
+if [ -n "$SDCARD_SIZE" ] && [ ! -f /data/android.avd/sdcard.img ]; then
+  echo "Creating SD card..."
+  # Create the SD card using the specified size
+  /opt/android-sdk/emulator/mksdcard $SDCARD_SIZE $SDCARD_PATH
+  echo "SD card created with size: $SDCARD_SIZE"
+fi
+
+chmod -x /opt/android-sdk/emulator/crashpad_handler
 # Start the emulator with the appropriate ramdisk.img
-/opt/android-sdk/emulator/emulator -avd android -writable-system -no-window -no-audio -no-boot-anim -skip-adb-auth -gpu swiftshader_indirect -no-snapshot $RAMDISK_PATH -qemu -m ${RAM_SIZE:-4096}
+/opt/android-sdk/emulator/emulator -avd android -writable-system -no-window -no-audio -no-boot-anim -skip-adb-auth -gpu swiftshader_indirect -no-snapshot -no-metrics $RAMDISK ${SDCARD_SIZE:+$SDCARD} -qemu -m ${RAM_SIZE:-4096}
