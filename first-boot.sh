e86a70875ea6ea0f9b4a18722920b53bb72cf2ae
Haroun EL ALAMI <haroun.dev@gmail.com>
2025-04-22 22:54:07 +0200

    fix(root): downgrade to magisk 26.4

--- first-boot.sh
+++ first-boot.sh
@@ -75,7 +75,7 @@ echo "Root Script Starting..."
 # Root the VM
 git clone https://gitlab.com/newbit/rootAVD.git
 pushd rootAVD
-sed -i 's/read -t 10 choice/choice=2/' rootAVD.sh
+sed -i 's/read -t 10 choice/choice=1/' rootAVD.sh
 ./rootAVD.sh system-images/android-30/default/x86_64/ramdisk.img
 cp /opt/android-sdk/system-images/android-30/default/x86_64/ramdisk.img /data/android.avd/ramdisk.img
 popd
