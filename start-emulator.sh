3fd8b859736895405d69175f470efd1961d9af98
Haroun EL ALAMI <haroun.dev@gmail.com>
2025-04-22 09:13:04 +0200

    feat: disable metrics and unlock net speed (#6)

--- start-emulator.sh
+++ start-emulator.sh
@@ -2,8 +2,8 @@
 
 # Check if the .first-boot-done file exists
 if [ -f /data/.first-boot-done ]; then
-  RAMDISK_PATH="-ramdisk /data/android.avd/ramdisk.img"
+  RAMDISK="-ramdisk /data/android.avd/ramdisk.img"
 fi
 
 # Start the emulator with the appropriate ramdisk.img
-/opt/android-sdk/emulator/emulator -avd android -writable-system -no-window -no-audio -no-boot-anim -skip-adb-auth -gpu swiftshader_indirect -no-snapshot $RAMDISK_PATH -qemu -m ${RAM_SIZE:-4096}
+/opt/android-sdk/emulator/emulator -avd android -nojni -netfast -writable-system -no-window -no-audio -no-boot-anim -skip-adb-auth -gpu swiftshader_indirect -no-snapshot -no-metrics $RAMDISK -qemu -m ${RAM_SIZE:-4096}
